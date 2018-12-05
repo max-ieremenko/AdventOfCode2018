c870dd49cc4f3e0261f8d3fc93447421530619c7
Maksym Ieremenko <maksym.ieremenko@qoniac.com>
2018-12-05 10:43:10 +0100

    Day 5

---
+++ Day5/Task1.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace Day5
+{
+    public static class Task1
+    {
+        public static int Solve(string input)
+        {
+            var test = new StringBuilder(input.Trim());
+            React(test);
+
+            return test.Length;
+        }
+
+        internal static bool AreOpposite(char x, char y)
+        {
+            if (char.IsUpper(x))
+            {
+                if (!char.IsLower(y))
+                {
+                    return false;
+                }
+
+                return x.Equals(char.ToUpperInvariant(y));
+            }
+
+            if (!char.IsUpper(y))
+            {
+                return false;
+            }
+
+            return x.Equals(char.ToLowerInvariant(y));
+        }
+
+        internal static void React(StringBuilder input)
+        {
+            var length = input.Length;
+
+            for (var i = 0; i < input.Length - 1; i++)
+            {
+                if (AreOpposite(input[i], input[i + 1]))
+                {
+                    input.Remove(i, 2);
+                    i--;
+                }
+            }
+
+            if (input.Length != length)
+            {
+                React(input);
+            }
+        }
+    }
+}
---
+++ Day5/Tests.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Day5
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase("aA", "")]
+        [TestCase("abBA", "")]
+        [TestCase("abAB", "abAB")]
+        [TestCase("aabAAB", "aabAAB")]
+        [TestCase("dabAcCaCBAcCcaDA", "dabCBAcaDA")]
+        [TestCase("cxaFffFAXb", "cb")]
+        public void Task1React(string input, string expected)
+        {
+            var test = new StringBuilder(input);
+            Task1.React(test);
+            test.ToString().ShouldBe(expected);
+        }
+
+        [Test]
+        [TestCase("dabAcCaCBAcCcaDA\n", 4)]
+        public void Task2Solve(string input, int expected)
+        {
+            Task2.Solve(input).ShouldBe(expected);
+        }
+
+        [Test]
+        [TestCase('a', 'A', true)]
+        [TestCase('A', 'a', true)]
+        [TestCase('a', 'a', false)]
+        [TestCase('a', 'b', false)]
+        public void AreOpposite(char x, char y, bool expected)
+        {
+            Task1.AreOpposite(x, y).ShouldBe(expected);
+        }
+    }
+}
