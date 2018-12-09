91050482b2ee29917ce313da70b4a15defa42dd9
max_ieremenko <max.ieremenko@gmail.com>
2018-12-09 17:10:43 +0100

    Day 9

---
+++ Day9/Marble.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace Day9
+{
+    [DebuggerDisplay("{Number}")]
+    internal sealed class Marble
+    {
+        public Marble(int number)
+        {
+            Number = number;
+        }
+
+        public int Number { get; }
+
+        public Marble Next { get; set; }
+
+        public Marble Previous { get; set; }
+    }
+}
